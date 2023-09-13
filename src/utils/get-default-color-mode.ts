4c53c56c105a2110ecc5fa5dcb1080190df0d814
Lulwah Almisfer <67158449+LulwahAlmisfer@users.noreply.github.com>
2023-09-13 11:56:26 +0300

    fixes

--- src/utils/get-default-color-mode.ts
+++ src/utils/get-default-color-mode.ts
@@ -1,14 +1,5 @@
 const getDefaultColorMode = (): "dark" | "light" => {
-  if (typeof window === "undefined") {
-    return "light";
-  }
-
-  const mql = window.matchMedia("(prefers-color-scheme: dark)");
-  const hasMediaQueryPreference = typeof mql.matches !== "undefined";
-  if (hasMediaQueryPreference) {
-    return mql.matches ? "dark" : "light";
-  }
-
+ 
   return "light";
 };
 
